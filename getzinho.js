93c30951224365a0079b960cf2cb7cce174b7430
gabrielsouzahub <gabriel_souza@unigranrio.br>
2021-03-29 22:45:38 -0300

    atualizando toDo e user

--- getzinho.js
+++ getzinho.js
@@ -1,7 +1,14 @@
+const { response } = require('express');
+
 const app = require('express')();
+const port = 3000;
 
 app.get('/todo',(_, response)=>{
-    response.send("Rota ativada com GET e recurso toDo valores de toDo devem ser retornados");
+    response.send(`Rota ativada com GET e recurso toDo valores de toDo devem ser retornados`);
 });
 
-app.listen("3000", console.log("toDo ligado"));
+app.get('/user',(_,response)=>{
+    response.send(`Rota ativada com GET e recurso user valores de user devem ser retornados`)
+})
+
+app.listen(port, console.log("toDo ligado"));
